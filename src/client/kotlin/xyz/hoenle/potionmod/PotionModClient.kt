8b41b33f8fb405fb59b43e2b9ca02729e991c4fa
Ruben Hoenle <git@hoenle.xyz>
2025-02-11 23:00:33 +0100

    initial commit

---
+++ src/client/kotlin/xyz/hoenle/potionmod/PotionModClient.kt
@@ -0,0 +1,9 @@
+package xyz.hoenle.potionmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object PotionModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
